fc0bcc3155846f651417c646bd76c1393e5b8adf
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-02 01:53:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,35 +21,20 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 cnn = load_model('v4_melanoma')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.title('Welcome To CAC APP WORK PLEASE!')
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+file = st.file_uploader('Upload An Image')
+if file:  # if user uploaded file
+        img = Image.open(file)
+        prediction = get_prediction(img)
+        st.write(prediction)
     
 
-def get_prediction(path):
-    
+def get_prediction(img):
     dimension = 224
     channels = 3
     
-    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     arr = np.ndarray(shape=(1, dimension, dimension, channels), dtype=np.float32)
     image = img
 
