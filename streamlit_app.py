8f2031f5e2f13306e6e93e9bf9346d30ccc6d5d3
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-08 00:36:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,7 @@ def get_hospitals():
             st.subheader(f"{place.name}")
             imgID = place.photos[0].photo_reference
             st.write(imgID)
+            st.write(str(place.photos))
             link = (
                 "https://maps.googleapis.com/maps/api/place/photo?photo_reference="
                 + imgID
