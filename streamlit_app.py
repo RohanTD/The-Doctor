b90fe40a514f64c54cfa04aeffeb1ffecacefdc0
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-07 19:19:42 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from googleplaces import GooglePlaces, types, lang
 from collections import namedtuple
 import altair as alt
 import math
@@ -7,6 +8,9 @@ import numpy as np
 from keras.models import load_model
 from PIL import Image
 import cv2
+import requests
+import json
+from googleplaces import GooglePlaces, types, lang
 """
 # Welcome to Streamlit!
 
@@ -18,10 +22,11 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+
 def get_prediction(img):
     dimension = 224
     channels = 3
-    
+
     img = img.convert("RGB")
     img = np.asarray(img)
     arr = np.ndarray(shape=(1, dimension, dimension,
@@ -37,15 +42,48 @@ def get_prediction(img):
     cnn = load_model('v4_melanoma')
     return cnn.predict(arr)
 
+
+def get_hospitals():
+    a = 'AIzaSyDCd_LRkdU3mHBQ01PY9zSxNat6AI_oD1M'
+    range = 10000  # in miles
+    r = requests.post(
+        fr'https://www.googleapis.com/geolocation/v1/geolocate?key={a}')
+
+    response = json.loads(r.content)
+
+    print(f'(Location accurate to {response["accuracy"]/1609} miles.)\n')
+    places = GooglePlaces(a)
+    query_result = places.nearby_search(lat_lng={
+        'lat': response['location']['lat'], 'lng': response['location']['lng']}, radius=range*1609, types=[types.TYPE_HOSPITAL])
+
+    if query_result.has_attributions:
+        print(query_result.html_attributions)
+    if len(query_result.places) == 0:
+        print(f"There are no hospitals in a {str(range)} mile proximity.")
+
+    results = []
+    for i, place in enumerate(query_result.places):
+        place.get_details()
+
+        results.append({'name': place.name, 'formatted_address': place.formatted_address,
+                        'website': place.website, 'gmapsURL': place.url})
+
+    print(f'({i}) : {place.name}')
+    print(f'  -  {place.formatted_address}')
+    print(f'  -  {place.url}')
+    print(f'  -  {place.website}')
+
+
 if __name__ == '__main__':
-     st.title('Welcome To the Medical App!')
+    st.title('Welcome To the Medical App!')
 
-     file = st.file_uploader('Upload An Image (Melanoma Detection)')
-     if file:  # if user uploaded file   
-          img = Image.open(file)
-          prediction = get_prediction(img)
-          if prediction[0] < 0.5:
-               st.write("Melanoma - Confidence: " + str((1 - prediction[0]) * 100) + "%")
-          else:
-               st.write("Not Melanoma - Confidence: " + str((prediction[0]) * 100) + "%")
-
+    file = st.file_uploader('Upload An Image (Melanoma Detection)')
+    if file:  # if user uploaded file
+        img = Image.open(file)
+        prediction = get_prediction(img)
+        if prediction[0] < 0.5:
+            st.write("Melanoma - Confidence: " +
+                     str((1 - prediction[0]) * 100) + "%")
+        else:
+            st.write("Not Melanoma - Confidence: " +
+                     str((prediction[0]) * 100) + "%")
