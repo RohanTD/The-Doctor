179c44c1c2de63bf8a981f8b44939a0e97a479f0
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-08 01:41:14 -0500

    created

--- Home.py
+++ Home.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.set_page_config(page_title="Home", page_icon="")
 st.markdown("# Home")
 st.sidebar.markdown("# Home")
+
+st.write(
+    "Welcome! This app is a univeral medical diagnostic tool with a range of functionality."
+)
+st.write(
+    "Choose a page to the left most consistent with the particular problem you are having, and you can find more information on the individual screens. On the final page, we can help you locate the nearest hospital in case you need to seek medical attention."
+)
