f0136a668a518cfaa665df079b036bb3428ffa18
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-08 00:51:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ def get_hospitals():
             st.subheader(f"{place.name}")
 
             #  for j in range(int(place.rating)):
-            st.image("star TP.png", width=50, height=50)
+            st.image("star TP.png", width=50)
             # st.write(f"Rating: {place.rating}")
             # st.write(f"Phone: {place.formatted_phone_number}")
             st.write(f"Address: {place.formatted_address}")
