f11993b547d671615fac4b8c932ed199f01e3481
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-07 19:28:23 -0500

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ import cv2
 import requests
 import json
 from googleplaces import GooglePlaces, types, lang
+
 """
 # Welcome to Streamlit!
 
@@ -29,8 +30,7 @@ def get_prediction(img):
 
     img = img.convert("RGB")
     img = np.asarray(img)
-    arr = np.ndarray(shape=(1, dimension, dimension,
-                     channels), dtype=np.float32)
+    arr = np.ndarray(shape=(1, dimension, dimension, channels), dtype=np.float32)
     image = img
 
     size = (dimension, dimension)
@@ -39,51 +39,61 @@ def get_prediction(img):
     image_array = np.asarray(image)
     arr[0] = (image_array.astype(np.float32) / 127.0) - 1
 
-    cnn = load_model('v4_melanoma')
+    cnn = load_model("v4_melanoma")
     return cnn.predict(arr)
 
 
 def get_hospitals():
-    a = 'AIzaSyDCd_LRkdU3mHBQ01PY9zSxNat6AI_oD1M'
+    a = "AIzaSyDCd_LRkdU3mHBQ01PY9zSxNat6AI_oD1M"
     range = 10000  # in miles
-    r = requests.post(
-        fr'https://www.googleapis.com/geolocation/v1/geolocate?key={a}')
+    r = requests.post(rf"https://www.googleapis.com/geolocation/v1/geolocate?key={a}")
 
     response = json.loads(r.content)
 
-    print(f'(Location accurate to {response["accuracy"]/1609} miles.)\n')
+    st.write(f'(Location accurate to {response["accuracy"]/1609} miles.)\n')
     places = GooglePlaces(a)
-    query_result = places.nearby_search(lat_lng={
-        'lat': response['location']['lat'], 'lng': response['location']['lng']}, radius=range*1609, types=[types.TYPE_HOSPITAL])
+    query_result = places.nearby_search(
+        lat_lng={
+            "lat": response["location"]["lat"],
+            "lng": response["location"]["lng"],
+        },
+        radius=range * 1609,
+        types=[types.TYPE_HOSPITAL],
+    )
 
     if query_result.has_attributions:
-        print(query_result.html_attributions)
+        st.write(query_result.html_attributions)
     if len(query_result.places) == 0:
-        print(f"There are no hospitals in a {str(range)} mile proximity.")
+        st.write(f"There are no hospitals in a {str(range)} mile proximity.")
 
     results = []
     for i, place in enumerate(query_result.places):
         place.get_details()
 
-        results.append({'name': place.name, 'formatted_address': place.formatted_address,
-                        'website': place.website, 'gmapsURL': place.url})
+        results.append(
+            {
+                "name": place.name,
+                "formatted_address": place.formatted_address,
+                "website": place.website,
+                "gmapsURL": place.url,
+            }
+        )
 
-    print(f'({i}) : {place.name}')
-    print(f'  -  {place.formatted_address}')
-    print(f'  -  {place.url}')
-    print(f'  -  {place.website}')
+    st.write(f"({i}) : {place.name}")
+    st.write(f"  -  {place.formatted_address}")
+    st.write(f"  -  {place.url}")
+    st.write(f"  -  {place.website}")
 
 
-if __name__ == '__main__':
-    st.title('Welcome To the Medical App!')
+if __name__ == "__main__":
+    st.title("Welcome To the Medical App!")
 
-    file = st.file_uploader('Upload An Image (Melanoma Detection)')
+    file = st.file_uploader("Upload An Image (Melanoma Detection)")
     if file:  # if user uploaded file
         img = Image.open(file)
         prediction = get_prediction(img)
         if prediction[0] < 0.5:
-            st.write("Melanoma - Confidence: " +
-                     str((1 - prediction[0]) * 100) + "%")
+            st.write("Melanoma - Confidence: " + str((1 - prediction[0]) * 100) + "%")
         else:
-            st.write("Not Melanoma - Confidence: " +
-                     str((prediction[0]) * 100) + "%")
+            st.write("Not Melanoma - Confidence: " + str((prediction[0]) * 100) + "%")
+	get_hospitals()
