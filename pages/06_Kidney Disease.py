710011ed02c4c8d3cd9b154eb7e33de5d98ccfa6
mvideet <43703950+mvideet@users.noreply.github.com>
2022-10-09 13:54:38 -0500

    Update 06_Kidney Disease.py

--- pages/06_Kidney Disease.py
+++ pages/06_Kidney Disease.py
@@ -36,6 +36,10 @@ htn = st.checkbox("Do you have hypertension?")
 dm = st.checkbox("Do you have diabetes mellitus?")
 cad = st.checkbox("Do you have coronary artery disease?")
 appet = st.selectbox("How is your appetite?", ["Good", "Poor"])
+if appet="Good":
+    appet =1
+else:
+    appet=0
 pe = st.checkbox("Do you have pedal edema?")
 ane = st.checkbox("Do you have anemia?")
 
