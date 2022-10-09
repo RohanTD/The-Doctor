d6a19ef1c97292d46753ee5f4f503f98ecc9df7f
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-09 13:08:13 -0500

    Update 06_Kidney Disease.py

--- pages/06_Kidney Disease.py
+++ pages/06_Kidney Disease.py
@@ -8,8 +8,33 @@ st.sidebar.markdown("# Kidney Disease  Predictor")
 st.write("Input your symptoms below")
 
 
-# ADD THE FORM
-
+age = st.number_input("How old are you?", value=20)
+bp = st.number_input("What is your diastolic blood pressure?", value=90)
+sg = st.selectbox(
+    "What is your specific gravity?", ["1.005", "1.010", "1.015", "1.020", "1.025"]
+)
+al = st.number_input(
+    "How severe is your albumin level?", value=0, max_value=5, min_value=0
+)
+su = st.number_input(
+    "How severe is your sugar level?", value=0, max_value=5, min_value=0
+)
+rbc = st.checkbox("Are your red blood cells normal normal?")
+pc = st.checkbox("Is your pus cell count normal?")
+pcc = st.checkbox("Are pus cell clumps present?")
+ba = st.checkbox("Are bacteria present?")
+bgr = st.number_input("What is your blood glucose level?", value=20)
+bu = st.number_input("What is your blood urea level?", value=100)
+sc = st.number_input("What is your serum creatinine level?", step=0.1)
+sod = st.number_input("What is your sodium level?", value=140)
+pot = st.number_input("What is your potassium level?", value=4, step=0.1)
+hemo = st.number_input("What is your hemoglobin level?", value=10, step=0.1)
+pcv = st.number_input("What is your packed cell volume?", value=40)
+wc = st.number_input("What is your white blood cell count?", value=10000, step=100)
+rc = st.number_input("What is your red blood cell count?", value=4, step=0.1)
+htn = st.checkbox("Do you have hypertension?")
+dm = st.checkbox("Do you have diabetes mellitus?")
+cad = st.checkbox("Do you have coronary artery disease?")
 
 rf = joblib.load("kidney.joblib")
 input_arr = [
