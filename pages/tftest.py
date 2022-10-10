4a74ed89672f57d33e0ad4950c95bf7a1cc1aaf4
Videet Mehta <43703950+mvideet@users.noreply.github.com>
2022-10-10 14:13:06 -0500

    Update tftest.py

--- pages/tftest.py
+++ pages/tftest.py
@@ -2,15 +2,44 @@ from keras.models import load_model
 import cv2
 import numpy as np
 import streamlit as st
+model = load_model('lyme.hdf5')
 
-model = load_model('lyme.h5')
+st.set_page_config(page_title="Lyme Disease Predictor", page_icon="")
+st.markdown("# Disease  Predictor")
+
+st.write(
+    "Upload an image of the suspected cancerous area. For best results, be sure that the growth is centered, well-lit, covers a majority of the image, and is minimally obstructed by hair"
+)
+
+def getImage(img):
+    img = cv2.resize(img,(128,128))
+    img = np.reshape(img,[1,128,128,3])
+    classes = model.predict(img)
+    if prediction[0] < 0.9:
+        st.write("Normal Rash") #- Confidence: " + str((1 - prediction[0]) * 100) + "%")
+    else:
+        st.write("Lyme - Confidence: " + str((prediction[0]) * 100) + "%")
 
 
+fileHold = st.empty()
+camHold = st.empty()
 
-img = cv2.imread('erythema migrans2.jpg')
-img = cv2.resize(img,(128,128))
-img = np.reshape(img,[1,128,128,3])
 
-classes = model.predict(img)
+def fileMethod():
+    fileHold.subheader("Upload")
+    file2 = fileHold.file_uploader("Upload an image")
+    if file2:  # if user uploaded file
+        camHold.empty()
+        getImage(Image.open(file2))
 
-print(classes)
+
+def camMethod():
+    camHold.subheader("Take photo")
+    cam = camHold.camera_input("Take a photo (please allow webcam access)")
+    if cam is not None:
+        fileHold.empty()
+        getImage(Image.open(cam))
+
+
+fileMethod()
+camMethod()
