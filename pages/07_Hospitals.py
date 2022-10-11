69b7f85ce4e2b09c6e437f8e584e307c709eecf4
Videet Mehta <43703950+mvideet@users.noreply.github.com>
2022-10-11 11:12:20 -0500

    Update 07_Hospitals.py

--- pages/07_Hospitals.py
+++ pages/07_Hospitals.py
@@ -5,7 +5,7 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.set_page_config(page_title="Hospital Finder", page_icon="")
+st.set_page_config(page_title="Hospital Finder", page_icon="🏥")
 st.markdown("# Hospital Finder")
 st.write(
     "Allow the site to access your location, and we will locate the nearest hospitals to you. Please be sure to disable any proxies or VPNs"
