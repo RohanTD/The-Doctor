93881ad67c9fd3529b95d01f5505d706c354467f
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-16 13:09:12 -0500

    Update 09_🧴Lyme.py

--- pages/09_🧴Lyme.py
+++ pages/09_🧴Lyme.py
@@ -3,7 +3,8 @@ import cv2
 from PIL import Image
 import numpy as np
 import streamlit as st
-model = load_model('lyme.hdf5')
+
+model = load_model("lyme.hdf5")
 confidence_val = 0.8
 st.set_page_config(page_title="Lyme Disease Predictor", page_icon="🧴")
 st.markdown("# Disease  Predictor")
@@ -16,31 +17,38 @@ st.write(
 #     img = np.reshape(img,[1,128,128,3])
 #     cnn = load_model("lyme.hdf5")
 #     return cnn.predict(img)
-confidence_val = st.slider('Choose your confidence value. The larger the number, the harder it will be to detect and will produce more false negatives. The lower the number, the algorithm will produce more false positives.', 0.75, 0.98, 0.01)
+confidence_val = st.slider(
+    "Choose your confidence value. The larger the number, the harder it will be to detect and will produce more false negatives. The lower the number, the algorithm will produce more false positives.",
+    0.75,
+    0.98,
+    0.01,
+)
+
 
 def get_prediction(img):
-    #st.write(type(img))
+    # st.write(type(img))
     dimension = 128
     channels = 3
     img = np.asarray(img)
-    #st.write(type(img))
+    # st.write(type(img))
     size = (dimension, dimension)
     img = cv2.resize(img, size)
-    img = np.reshape(img,[1, 128, 128, channels])
-    
-    
+    img = np.reshape(img, [1, 128, 128, channels])
 
-#     image_array = np.asarray(image)
-#     arr[0] = (image_array.astype(np.float32) / 127.0) - 1
+    #     image_array = np.asarray(image)
+    #     arr[0] = (image_array.astype(np.float32) / 127.0) - 1
 
     cnn = load_model("lyme.hdf5")
     return cnn.predict(img)
 
+
 def getImage(img):
-    
+
     prediction = get_prediction(img)
-    if prediction[0] < confidence_val:
-        st.subheader("Tested Negative for Lyme Disease") #- Confidence: " + str((1 - prediction[0]) * 100) + "%")
+    if prediction[0] < (confidence_val * 0.75 + 0.25):
+        st.subheader(
+            "Tested Negative for Lyme Disease"
+        )  # - Confidence: " + str((1 - prediction[0]) * 100) + "%")
     else:
         st.subheader("Lyme at a Confidence of " + str((prediction[0]) * 100) + "%")
 
