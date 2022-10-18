d5ba67dd6bd0851b10d1b1e6c28adc942aa0f323
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-18 07:37:11 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="The Doctor", page_icon="")
 st.markdown("# The Doctor")
 
-st.image("logo cac.png")
 
 st.write(
     "Welcome! The Doctor is a univeral medical diagnostic tool with a range of functionality."
@@ -11,3 +10,4 @@ st.write(
 st.write(
     "Choose a page to the left most consistent with the particular problem you are having, and you can find more information on the individual screens. On the final page, we can help you locate the nearest hospital in case you need to seek medical attention."
 )
+st.image("logo cac.png", width=200)
