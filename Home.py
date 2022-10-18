a20578d5f415f89d39170601ca58af329e85b84e
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-18 08:00:39 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="The Doctor", page_icon="")
+st.set_page_config(page_title="The Doctor", page_icon="🏠")
 st.markdown("# The Doctor")
 
 st.image("logo cac.png")
