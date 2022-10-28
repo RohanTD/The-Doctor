0fda0fc3e222aa1106b1a46fc4a3d674e3d51440
Videet Mehta <mvideet@gmail.com>
2022-10-27 19:00:24 -0500

    Update 09_🦠General Disease.py

--- pages/09_🦠General Disease.py
+++ pages/09_🦠General Disease.py
@@ -154,6 +154,12 @@ Data = "Training.csv"
 data = pd.read_csv(Data).dropna(axis=1)
 encoder = LabelEncoder()
 data = data[data.prognosis !="AIDS"]
+data = data[data.prognosis!= "Drug Reaction"]
+data = data[data.prognosis!= "Fungal infection"]
+
+data = data[data.prognosis!= "Paralysis (brain hemorrhage)"]
+data = data[data.prognosis!= "Heart attack"]
+
 st.write(data)
 data["prognosis"] = encoder.fit_transform(data["prognosis"])
 symptom_index = {}
