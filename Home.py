77ce0ffb1d266088f488a1c3e134ac06ad5b5b88
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-29 13:10:18 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,3 +10,18 @@ st.write(
 st.write(
     "Choose a page to the left most consistent with the particular problem you are having, and you can find more information on the individual screens. On the final page, we can help you locate the nearest hospital in case you need to seek medical attention."
 )
+
+footer = """
+<style>
+footer{
+    visibility:visible;
+}
+footer:after{
+    content:'Please keep in mind that this is a machine learning algorithm-based predictor. Although it is highly accurate, false positive or negative results can occur. If you still have concerns after consulting our app, please contact your doctor or find a hospital using our locator tool.';
+    display:block;
+    position:relative;
+}
+</style>
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
