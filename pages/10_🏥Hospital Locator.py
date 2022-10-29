e5642219a569fd892ee05f5e80fe3f76f4295d89
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-10-29 13:17:40 -0500

    Update 10_🏥Hospital Locator.py

--- pages/10_🏥Hospital Locator.py
+++ pages/10_🏥Hospital Locator.py
@@ -9,7 +9,7 @@ from streamlit_bokeh_events import streamlit_bokeh_events
 st.set_page_config(page_title="Hospital Finder", page_icon="🏥")
 st.markdown("# Hospital Finder")
 st.write(
-    "Allow the site to access your location, and we will locate the nearest hospitals to you. Please be sure to disable any proxies or VPNs"
+    "Allow the site to access your location, and we will find the nearest hospitals to you. Please be sure to disable any proxies or VPNs"
 )
 
 
