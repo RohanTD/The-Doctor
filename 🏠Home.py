f1471999219e56f2a0d705cedad8992cbd4a813b
RohanTD <43703979+RohanTD@users.noreply.github.com>
2022-11-01 22:08:20 -0500

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="The Doctor", page_icon="🏠")
 st.markdown("# The Doctor")
 
-st.image("logo cac.png")
+st.image("logo.png")
 st.write(
     "Welcome! The Doctor is a univeral medical diagnostic tool with a range of functionality."
 )
